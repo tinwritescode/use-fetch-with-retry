10046a33de9044de8d684dc55e9d3544578ac4ff
Tin Nguyen <hopthucuatin@gmail.com>
2023-02-02 14:46:16 +0700

    initial commit

---
+++ hooks/useFetchWithRetry.js
@@ -0,0 +1,39 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const ERROR_TEXT = "Can't get data";
+export const useFetchWithReTry = ({ url, maxRetry = 3 }) => {
+    const [data, setData] = useState(null);
+    const [isLoading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        async function fetchData() {
+            let tryTime = maxRetry;
+
+            while (tryTime > 0) {
+                try {
+                    setLoading(true);
+                    const res = await axios.get(url, {
+                        headers: {
+                            "Content-Type": "application/json",
+                        },
+                    });
+                    const data = res.data;
+
+                    setData(data);
+                    return;
+                } catch (error) {
+                    tryTime -= 1;
+                }
+            }
+
+            if (tryTime === 0) setError(ERROR_TEXT);
+            setLoading(false);
+        }
+
+        fetchData();
+    }, [maxRetry, url]);
+
+    return { data, isLoading, error };
+};
