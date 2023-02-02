42afdfa4107c32559c3421f89caf1f5eb94b70f8
tinwritescode <hopthucuatin@gmail.com>
2023-02-02 14:59:49 +0700

    fix: not set loading to true at the end

--- hooks/useFetchWithRetry.js
+++ hooks/useFetchWithRetry.js
@@ -1,39 +1,40 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const ERROR_TEXT = "Can't get data";
 export const useFetchWithReTry = ({ url, maxRetry = 3 }) => {
-    const [data, setData] = useState(null);
-    const [isLoading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
+  const [data, setData] = useState(null);
+  const [isLoading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-    useEffect(() => {
-        async function fetchData() {
-            let tryTime = maxRetry;
+  useEffect(() => {
+    async function fetchData() {
+      let tryTime = maxRetry;
 
-            while (tryTime > 0) {
-                try {
-                    setLoading(true);
-                    const res = await axios.get(url, {
-                        headers: {
-                            "Content-Type": "application/json",
-                        },
-                    });
-                    const data = res.data;
+      while (tryTime > 0) {
+        try {
+          setLoading(true);
+          const res = await axios.get(url, {
+            headers: {
+              'Content-Type': 'application/json',
+            },
+          });
+          const data = res.data;
 
-                    setData(data);
-                    return;
-                } catch (error) {
-                    tryTime -= 1;
-                }
-            }
-
-            if (tryTime === 0) setError(ERROR_TEXT);
-            setLoading(false);
+          setData(data);
+          setLoading(false);
+          return;
+        } catch (error) {
+          tryTime -= 1;
         }
+      }
 
-        fetchData();
-    }, [maxRetry, url]);
+      if (tryTime === 0) setError(ERROR_TEXT);
+      setLoading(false);
+    }
 
-    return { data, isLoading, error };
+    fetchData();
+  }, [maxRetry, url]);
+
+  return { data, isLoading, error };
 };
--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,18 +1,27 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  const mockApi = { "country_code": "VN", "country_name": "Vietnam", "city": null, "postal": null, "latitude": 16, "longitude": 106, "IPv4": "116.100.195.126", "state": null }
+  const mockApi = {
+    country_code: 'VN',
+    country_name: 'Vietnam',
+    city: null,
+    postal: null,
+    latitude: 16,
+    longitude: 106,
+    IPv4: '116.100.195.126',
+    state: null,
+  };
 
   // rand 70% success
   const rand = Math.random() * 100;
-  const isError = true;
-
-  if (isError) {
-    res.status(500).json({ error: 'Internal server error' })
-    return;
-  }
+  const isError = rand < 60;
 
   setTimeout(() => {
-    res.status(200).json(mockApi)
-  }, 3000)
+    if (isError) {
+      res.status(500).json({ error: 'Internal server error' });
+      return;
+    }
+
+    res.status(200).json(mockApi);
+  }, 3000);
 }
